e0392791592fe1c973a606bd424976c6b24fe9fd
Mitchell Goff <mitchellgoffpc@gmail.com>
2024-05-28 23:15:19 -0700

    Too clever by half

--- src/main.rs
+++ src/main.rs
@@ -46,23 +46,19 @@ fn concat<T: Clone>(vec1: &[T], vec2: &[T]) -> Vec<T> {
     vec1.iter().cloned().chain(vec2.iter().cloned()).collect()
 }
 
-fn get_pair_frequencies(data: &[usize]) -> HashMap<[usize; 2], usize> {
-    let mut pair_frequencies: HashMap<usize, usize> = HashMap::new();
+fn get_pair_frequencies(data: &[usize]) -> HashMap<(usize, usize), usize> {
+    let mut pair_frequencies: HashMap<(usize, usize), usize> = HashMap::new();
     for i in 0..data.len() - 1 {
-        *pair_frequencies.entry(data[i] + data[i + 1] << 32).or_insert(0) += 1;
+        *pair_frequencies.entry((data[i], data[i + 1])).or_insert(0) += 1;
     }
-    pair_frequencies.into_iter().map(|(key, value)| {
-        let a = key & 0xFFFFFFFF;
-        let b = key >> 32;
-        ([a, b], value)
-    }).collect()
+    pair_frequencies
 }
 
-fn merge(data: &[usize], ids: [usize; 2], idx: usize) -> Vec<usize> {
+fn merge(data: &[usize], ids: (usize, usize), idx: usize) -> Vec<usize> {
     let mut merged_data = Vec::new();
     let mut i = 0;
     while i < data.len() {
-        if i < data.len() - 1 && data[i] == ids[0] && data[i + 1] == ids[1] {
+        if i < data.len() - 1 && data[i] == ids.0 && data[i + 1] == ids.1 {
             merged_data.push(idx);
             i += 2;
         } else {
@@ -84,7 +80,7 @@ fn train(bytes: &[u8], n: usize) -> Vec<(usize, usize, usize)> {
         let pair_frequencies = get_pair_frequencies(&data);
         let max_pair = pair_frequencies.iter().max_by_key(|&(_, count)| count).map(|(pair, _)| *pair).unwrap();
         data = merge(&data, max_pair, 256 + i);
-        merges.push((256 + i, max_pair[0], max_pair[1]));
+        merges.push((256 + i, max_pair.0, max_pair.1));
     }
 
     merges
@@ -93,7 +89,7 @@ fn train(bytes: &[u8], n: usize) -> Vec<(usize, usize, usize)> {
 fn encode(bytes: &[u8], merges: &Vec<(usize, usize, usize)>) -> Vec<usize> {
     let mut data: Vec<usize> = bytes.iter().map(|&x| x as usize).collect();
     for (index, a, b) in merges.iter() {
-        data =  merge(&data, [*a, *b], *index);
+        data =  merge(&data, (*a, *b), *index);
     }
     data
 }
@@ -124,7 +120,6 @@ fn main() {
             let encoded = encode(&bytes, &merges);
             let decoded = decode(&encoded, &merges);
             assert_eq!(bytes, decoded);
-
         },
         "encode" => {
             let bytes = read(&args.input);
